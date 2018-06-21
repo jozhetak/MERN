d4f0db15fad37e025641261b36f14c489db27656
laurent <laurent.sama@gmail.com>
2018-06-21 19:10:18 +0200

    Save user to db

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+const User = require('../../models/User');
 
 router.get('/test', (req, res) => res.json({toto: "toto"}) );
+
+// @route api/users/register
+router.post('/register', (req, res) => {
+    User.findOne({ email: req.body.email })
+        .then( user => {
+            if (user) {
+                return res.status(400).json({email: "Email already exists"});
+            }
+            const avatar = gravatar.url(req.body.email, {
+                s: '200', //size
+                r: 'pg', // rating
+                d: 'mm' //default
+            });
+
+            user = new User({
+                name: req.body.name,
+                email: req.body.email,
+                avatar,
+                password: req.body.password
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(user.password, salt, (err, hash) => {
+                    if (err) {
+                        throw err;
+                    }
+
+                    user.password = hash;
+                    user.save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
+                });
+            });
+        })
+        .catch(err => console.log(err));
+});
 
 module.exports = router;
