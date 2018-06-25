379d8fe2ab08fd9e2b4657a3b97a81c9379c0320
laurent <laurent.sama@gmail.com>
2018-06-25 06:29:28 +0200

    login validation

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://laurent:tokisama1@ds163730.mlab.com:63730/traversy"
+    mongoURI: "mongodb://laurent:tokisama1@ds163730.mlab.com:63730/traversy",
+    secretOrKey: 'secret'
 };
--- server.js
+++ server.js
@@ -4,19 +4,20 @@ const bodyParser = require('body-parser');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
-
+const passport = require('passport');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello Toto'));
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 app.use('/api/users', users);
 app.use('/api/profile', profile);
-
 app.use('/api/posts', posts);
+
 const db = require('./config/keys').mongoURI;
 mongoose.connect(db)
     .then(() => console.log('Mongo connected'))
