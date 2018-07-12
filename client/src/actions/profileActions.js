5c92473546d4b0f8ee9689a069048f9a9d98bbe1
laurent <laurent.sama@gmail.com>
2018-07-12 19:12:32 +0200

    Hooked up GET Profiles routes and components

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE, SET_CURRENT_USER } from './types';
+import { GET_PROFILE, GET_PROFILES, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE, SET_CURRENT_USER } from './types';
 
 // get current profile
 
@@ -98,6 +98,25 @@ export const deleteEducation = id => dispatch =>{
             dispatch ({
                 type: GET_ERRORS,
                 payload: err.response.data
+            });
+        });
+}
+
+
+export const getProfiles = () => dispatch => {
+    dispatch(setProfileLoading());
+
+     axios.get(`/api/profile/all`)
+        .then(res =>
+            dispatch({
+                type: GET_PROFILES,
+                payload: res.data
+            })
+        )
+        .catch(err => {
+            dispatch ({
+                type: GET_PROFILES,
+                payload: null
             });
         });
 }
