08b33dd686b3f0ca7b2c2f9f061a49ea580982d0
laurent <laurent.sama@gmail.com>
2018-07-18 12:24:27 +0200

    Add deployment script

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 const passport = require('passport');
-
+const path = require('path');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -17,6 +17,16 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+
+// Static assets for prod
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 
 const db = require('./config/keys').mongoURI;
 mongoose.connect(db)
